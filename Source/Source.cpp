e927ea13c6b00f1b5e67f468be97b4403d295d8f
ArtemS18 <65869064+ArtemS18@users.noreply.github.com>
2023-12-05 18:52:51 +0300

    Update Source.cpp

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -3,10 +3,6 @@
 #include "Stack.cpp"
 using namespace std;
 
-int defPriority(char);
-void addSpace(char* newS, int& k);
-
-
 void postfixNotation(char* s, char* newS, int len) {
 }
 void coutingValue(char* newS) {
