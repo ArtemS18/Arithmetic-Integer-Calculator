48e2c2695687a6a702033fcd2413955f55305f55
ArtemS18 <65869064+ArtemS18@users.noreply.github.com>
2023-12-05 19:06:05 +0300

    Update CheckLine.hpp

--- Source/CheckLine/CheckLine.hpp
+++ Source/CheckLine/CheckLine.hpp
@@ -1,1 +1,19 @@
-
+namespace checkLine{
+    bool cheakSymbol(char*, int);
+    void coutShift(char*, int, int&);
+    
+    int cheakInput(char* s, int Len) {
+        int newLen = Len;
+        return newLen;
+    }
+    void cheakModul(char* newStr, char* prevStr, int& prevPos, int& newPos, int &shift) {
+    }
+    void createNewStr(char* newStr, char* prevStr, int prevLen) {
+    }
+    
+    bool cheakSymbol(char* s, int i) {
+        return 0;
+    }
+    void coutShift(char* s, int i, int& newLen) {
+    }
+}
